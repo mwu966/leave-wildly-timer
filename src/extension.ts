ffa562f7f31cfd8fda9d76ea49f0ccde4e52e314
Colloid <c011319546@edu.teu.ac.jp>
2020-11-11 23:49:08 +0900

    Added show Current time

--- src/extension.ts
+++ src/extension.ts
@@ -7,37 +7,31 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-
 	console.log('Congratulations, your extension "leave-wildly-timer" is now active!');
 
 	let statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
-	statusBar.text = "test dayo";
 	statusBar.tooltip = "tool tip";
-
-	const yNoCommand = 'leave-wildly-timer.test';
-	context.subscriptions.push(vscode.commands.registerCommand(yNoCommand, () => {
-		vscode.window.showInformationMessage('koreha ixtutai nannnann?');
-		statusBar.text = "comando-";
-	}));
-	statusBar.command = yNoCommand;
 	statusBar.show();
 	context.subscriptions.push(statusBar);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('leave-wildly-timer.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('leave-wildly-timer.time', () => {
 		// The code you place here will be executed every time your command is executed
-
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from leave-wildly-timer!');
 	});
 
 	context.subscriptions.push(disposable);
-
+	setInterval(() => {
+		let date2 = new Date();
+		statusBar.text = date2.getHours() + ":" + date2.getMinutes() + " " + date2.getSeconds();
+	}, 1000);
 
 
 }
+
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
